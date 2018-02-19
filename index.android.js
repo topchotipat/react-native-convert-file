d71a32d6b218934e94471d37e4021d74347740a6
Chotipat Top <topz.classic@gmail.com>
2018-02-19 18:27:41 +0700

    Create index android

---
+++ index.android.js
@@ -0,0 +1,17 @@
+/**
+ * Stub of movToMp4 for Android.
+ *
+ * @providesModule movToMp4
+ * @flow
+ */
+'use strict';
+
+var warning = require('fbjs/lib/warning');
+
+var RNConvertFile = {
+  test: () => {
+    warning('Not yet implemented for Android.');
+  }
+};
+
+module.exports = RNConvertFile;
